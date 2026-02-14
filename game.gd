ac976ffbfd95392d2b2d9d08b5acf327c45df5f8
Flatweb <flatwebali@gmx.com>
2026-02-14 13:43:26 +0100

    Première ébauche avec gridmap 2 sols, oiseau basique, caméra 3/4, mouvement en avant et latéral

---
+++ game.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+func _process(_delta):
+	#print ("vvvvvvv")
+	#print ($Oiseau.rotation.z)
+	#print ($Camera3D.rotation.z)
+	#print ("^^^^^^^^")
+	pass
---
+++ oiseau.gd
@@ -0,0 +1,29 @@
+extends CharacterBody3D
+
+# Vitesse de rotation sur virage
+const ROTSPEED = 0.1
+# Vitesse de rotation sur retour naturel à la position stable
+const ROTBACKSPEED = 0.02
+
+# Vitesse de vol horizontal
+var speed : float = 4.0
+# Vitesse de vol latéral
+var speedlat : float = 2.0
+ 
+func _ready():
+	pass
+
+func _physics_process(delta: float) -> void:
+	var change = Input.get_axis("droite","gauche")
+	
+	if change:
+		if abs(rotation.z) < PI/4 :
+			self.rotate_z(change*ROTSPEED*delta)
+	else:
+		if abs(rotation.z) < ROTBACKSPEED*delta :
+			rotation.z = 0
+		else:
+			self.rotate_y(-sign(rotation.z)*ROTBACKSPEED*delta)
+	
+	var moving : Vector3 = Vector3(change*speedlat*delta,0,-speed*delta)
+	move_and_collide(moving)
