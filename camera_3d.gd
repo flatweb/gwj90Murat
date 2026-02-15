1622fda81bc5aeb91e7910da0ee58f075b26d397
Flatweb <flatwebali@gmx.com>
2026-02-15 08:27:55 +0100

    mouvement latéral de la caméra pour suivre l'oiseau qui va trop vers la gauche ou la droite #7

--- camera_3d.gd
+++ camera_3d.gd
@@ -13,7 +13,6 @@ var followed : Node3D :
 # Note : la caméra a un Z > followed, distancez > 0
 var distancez : float
 # distance sur l'axe x à respecter (à l'écart ECART_MAX_X près)
-# Note : la caméra a un Z > followed, distancez > 0
 var distancex : float
 # écart absolu max toléré pour le suivi sur l'axe des x
 const ECART_MAX_X = 5.0
@@ -28,7 +27,13 @@ func _process(delta: float) -> void:
 	# Si la caméra est trop loin derrière, on avance la caméra
 	if self.position.z - followed.position.z > distancez:
 		self.position.z = followed.position.z + distancez
-	# Si on est troip près on ne fait rien
+	# Si on est trop près on ne fait rien
 	
+	# Si la caméra est trop décalé en X, on va suivre
+	if abs(self.position.x - followed.position.x - distancex) > ECART_MAX_X:
+		# signe de l'écart (camera trop à gauche > 0, caméra trop à droite < 0)
+		var sign_ecart = sign(self.position.x - followed.position.x - distancex)
+		# on recale la caméra sur l'écartmax
+		self.position.x = followed.position.x + distancex + ECART_MAX_X * sign_ecart
 	# TODO : gérer aussi le latéral
 	
