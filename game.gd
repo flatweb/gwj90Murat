a3ba433a2718b7955579ef6ceac599058319fccf
Flatweb <flatwebali@gmx.com>
2026-02-16 16:09:04 +0100

    Peuplement automatique des cellules du gridmap vides #41 Application d'une rotation aléatoire sur les mesh posées dans la gridmap #41 Modification de la meshlib pour avoir des formes non régulières

--- game.gd
+++ game.gd
@@ -2,17 +2,16 @@ extends Node3D
 
 #Par défaut, on considère que le gridmap sera de la même size que le $MeshGround
 var gridmapSize : Vector2
-var zoneforet = Vector2(0,20)
-var zoneprairie = Vector2(21,40)
-var zones = [zoneforet, zoneprairie]
-var ids : Array
 
 func _ready():
 	gridmapSize = $Ground/MeshGround.mesh.size
-	$Oiseau.set_limite_x(gridmapSize.x*0.75)
-	#populategridmap()
+	populategridmap()
+	
 	$Ground/CollisionGround.shape.size.x = gridmapSize.x
 	$Ground/CollisionGround.shape.size.z = gridmapSize.y
+	
+	$Oiseau.set_limite_x(gridmapSize.x*0.75)
+	
 	startintro()
 	pass
 
@@ -27,34 +26,60 @@ func start():
 	$AudioStreamPlayer.play()
 
 func populategridmap():
-	var foretsid : Array
-	var prairiesid : Array
+	var zones : Dictionary[String,Vector2]
+	
+	var startmapy = gridmapSize.y/2
+	zones = {"Foret":Vector2(startmapy-20,startmapy-0), \
+			"Prairie":Vector2(startmapy-40,startmapy-21)  \
+			}
+	
+	var tabindex : Dictionary[String,Array]
+	
+	
 	for child in get_children():
 		var gmap : GridMap
 		if not child is GridMap: continue # Ne prend que les enfants GridMap
 		
 		gmap = child as GridMap
+	
+		var rotations = Array()
+		for q in range(0,4):
+			rotations.append(gmap.get_orthogonal_index_from_basis(Basis.IDENTITY.rotated(Vector3.UP,PI/2*q)))
+		
 		var meshlib : MeshLibrary = gmap.mesh_library
-		for amesh in meshlib.get_item_list() :
-			var meshname = meshlib.get_item_name(amesh)
-			if meshname.begins_with("Foret"):
-				foretsid.append(amesh)
-			elif meshname.begins_with("Prairie"):
-				prairiesid.append(amesh)
+		for imesh in meshlib.get_item_list() :
+			var meshname = meshlib.get_item_name(imesh)
+			for zone in zones :
+				if meshname.begins_with(zone):
+					var array : Array
+					if tabindex.has(zone):
+						array = tabindex.get(zone)
+					else:
+						array = Array()
+					array.append(imesh)
+					tabindex[zone] = array
 			
-			for x in range(0,gridmapSize.x):
-				for y in range(0,gridmapSize.y) :
-					var vect3 = Vector3(x,0,y)
-					var index = gmap.get_cell_item(vect3)
-					if index != GridMap.INVALID_CELL_ITEM : continue
-				
-					# Nouvelle valeur de l'index
-					for zone in zones :
-						if y <zone.x or y > zone.y : continue
+		print (tabindex)
+		#var cells = gmap.get_used_cells()
+			
+		for x in range(-gridmapSize.x/2,+gridmapSize.x/2):
+			for y in range(gridmapSize.y/2,-gridmapSize.y/2,-1) :
+				var vect3 = Vector3(x,0,y)
+				var index = gmap.get_cell_item(vect3)
+				if index != GridMap.INVALID_CELL_ITEM :
+					continue
+			
+				# Nouvelle valeur de l'index
+				for zone in zones :
+					# On utilise des limites de zones éventuellement inversées
+					if y >=zones[zone].x and y < zones[zone].y  or \
+						y >=zones[zone].y and y < zones[zone].x :
 						# On a trouvé la bonne zone
-						index = zone[randi_range(0,foretsid.size()-1)]
-						gmap.set_cell_item(vect3,index)
-						
+						# on prend l'un des meshs au hasard parmi ceux de la zone
+						index = tabindex[zone][randi_range(0,tabindex[zone].size()-1)]
+						# on positionne
+						gmap.set_cell_item(vect3, index, rotations[randi_range(0,3)])
+					
 		gmap.show()
 	
 	
