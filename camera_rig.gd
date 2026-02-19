45c33ac2f6318826509f8ec6483fa55daf26dcea
darkbean <darkbean01@gmail.com>
2026-02-19 11:50:30 +0100

    tweak camera_rig

--- camera_rig.gd
+++ camera_rig.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var joueur : CharacterBody3D
+@export var angle : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +10,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var joueurXZ = Vector3(joueur.position.x, 0, joueur.position.z)
+	#var joueurXZ = Vector3(joueur.position.x, 0, joueur.position.z)
 	#self.position = joueurXZ
+	#rotation.y = angle
+	var rot = Vector3(0, angle, 0)
+	set_rotation_degrees(rot)
+
 	self.position = joueur.position
