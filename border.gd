38febde2f20ab46456b26359c295f8173af9580f
PC-Pro <fablab@cocotte-numerique.fr>
2026-02-19 14:28:27 +0100

    Reglage des collisions avec les boids

---
+++ border.gd
@@ -0,0 +1,14 @@
+extends Area3D
+
+
+func _on_area_entered(area: Area3D) -> void:
+	#var areaEntering = Area3D.get_tree().get_parent()
+	#if areaEntering.is_in_group("isBoid"):
+	#	areaEntering.isOutOfBorder
+	pass
+
+func _on_area_exited(area: Area3D) -> void:
+	#var areaEntering = Area3D
+	#if areaEntering.is_in_group("isBoid"):
+	#	areaEntering.isOutOfBorder
+	pass
