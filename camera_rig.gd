096ca33f19215ea5b64cc538a64c11a7bf0a367e
Alexandre <darkbean01@gmail.com>
2026-02-22 00:52:27 +0100

    rotation de la camera avec clic droit, echap pour sortir du MOUSE_MODE_CAPTURED

--- camera_rig.gd
+++ camera_rig.gd
@@ -1,40 +1,46 @@
 extends Node3D
 
 @export var joueur : CharacterBody3D
-@export var angle : int
+@export var initial_angle : int
 @export var camera : Camera3D
-@export var camera_distance_default : float = 39 #defaut = 39
+@export var camera_distance_default : float = 35
 var camera_distance
-@export var camera_min_distance : int = 10
+@export var camera_min_distance : int = 7 # moins et on voit trop l'horizon
 @export var camera_max_distance : int = 60
 @export var zoom_increment : float = 3.0
+@export var mouse_sensitivity : float = 0.1
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	camera_distance = camera_distance_default
 	update_camera_distance()
-	pass # Replace with function body.
+	set_rotation_degrees(Vector3(0, initial_angle, 0)) # initial camera rotation
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#var joueurXZ = Vector3(joueur.position.x, 0, joueur.position.z)
-	#self.position = joueurXZ
-	#rotation.y = angle
-	var rot = Vector3(0, angle, 0)
-	set_rotation_degrees(rot)
-
 	self.position = joueur.position
 	
 	var zoom = camera_distance
 	if Input.is_action_just_pressed("scroll_down"):
-		zoom += zoom_increment
+		zoom += zoom_increment# ET REGARDE LE JOUEUR
 		update_camera_distance()
 	if Input.is_action_just_pressed("scroll_up"):
 		zoom -= zoom_increment
 		update_camera_distance()
 	camera_distance = clamp(zoom, camera_min_distance, camera_max_distance)
+	print(camera_distance)
 
+# CHANGE LA DISTANCE ENTRE LA CAMERA ET LE CAMERA RIG (qui a la meme position que le joueur)
 func update_camera_distance():
 	camera.position.y = camera_distance
-	camera.look_at(self.position)
+	camera.look_at(self.position) # la camera regarde l'orgine du CameraRig (qui a la meme position que le joueur)
+	
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("clic_droit"):
+		if event is InputEventMouseMotion:
+			rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+
+func _unhandled_input(event: InputEvent): 
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
