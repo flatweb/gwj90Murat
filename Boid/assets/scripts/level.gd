92fd75c6bfc4f72305d01662907f4faddf76d003
Orsovi <orso.solidari@gmail.com>
2026-02-22 13:22:34 +0100

    Ajout de plusieurs voitures par route, suppression de script inutile

--- Boid/assets/scripts/level.gd
+++ Boid/assets/scripts/level.gd
@@ -1,7 +1,7 @@
 extends Node3D
 @export var carScene: PackedScene
 @export var carResScript: Script
-
+@export var numberOfCarByRoad: int = 5
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#Charge les voitures sur les chemins avec le groupe isRoad
@@ -9,11 +9,14 @@ func _ready() -> void:
 	for road in _roads:
 		var carPathScript = carResScript.get_path()
 		var carScript = load(carPathScript)
-		var roadPathFollow = PathFollow3D.new()
-		roadPathFollow.set_script(carScript)
-		road.add_child(roadPathFollow)
-		var car = carScene.instantiate()
-		roadPathFollow.add_child(car)
+		for i in numberOfCarByRoad:
+			var roadPathFollow = PathFollow3D.new()
+			roadPathFollow.set_script(carScript)
+			road.add_child(roadPathFollow)
+			var car = carScene.instantiate()
+			roadPathFollow.add_child(car)
+			roadPathFollow.progress_ratio = randf()
+			
 		#print("Est ce que le pathCar a un script" + String(roadPathFollow.get_path()))
-		if roadPathFollow.get_script() == carScript :
-			print("oui")
+			if roadPathFollow.get_script() == carScript :
+				print("oui")
